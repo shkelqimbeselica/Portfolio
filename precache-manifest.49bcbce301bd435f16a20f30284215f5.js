9ba72b26ff379aa07953fee8da3a534cb4b613e8
shkelqimbeselica <beselicashkelqim@gmail.com>
2020-10-24 00:16:57 +0200

    Updates

---
+++ precache-manifest.49bcbce301bd435f16a20f30284215f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4790114df8e53f6c163e5df0ba63f6",
+    "url": "/shkelqimbeselica/index.html"
+  },
+  {
+    "revision": "4f0bb3a826386212052c",
+    "url": "/shkelqimbeselica/static/css/main.ffbff34c.chunk.css"
+  },
+  {
+    "revision": "e19cb9d551c715d5a256",
+    "url": "/shkelqimbeselica/static/js/2.e37e8877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shkelqimbeselica/static/js/2.e37e8877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0bb3a826386212052c",
+    "url": "/shkelqimbeselica/static/js/main.e35d5d4e.chunk.js"
+  },
+  {
+    "revision": "1e2332b17475a49ac9c3",
+    "url": "/shkelqimbeselica/static/js/runtime-main.e762d5a6.js"
+  }
+]);

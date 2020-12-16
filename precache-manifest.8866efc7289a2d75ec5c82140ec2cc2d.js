a019c12e06411d7b79db98046a96b3441304e6b2
shkelqimbeselica <beselicashkelqim@gmail.com>
2020-12-16 21:45:49 +0100

    Updates

---
+++ precache-manifest.8866efc7289a2d75ec5c82140ec2cc2d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f93ff9b59fb0b08ddaebbcf646b936",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "334267de34c92a58cbc3",
+    "url": "/Portfolio/static/css/main.9c210790.chunk.css"
+  },
+  {
+    "revision": "e66fef087521e435f093",
+    "url": "/Portfolio/static/js/2.e5704cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.e5704cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334267de34c92a58cbc3",
+    "url": "/Portfolio/static/js/main.f56ad2f6.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "bcf733a4ec075e9c16ff6aa69c52f8d2",
+    "url": "/Portfolio/static/media/Balkan Cuisine.bcf733a4.webp"
+  },
+  {
+    "revision": "4c64eb14bef085d1ee7f8f1037c33533",
+    "url": "/Portfolio/static/media/Listify.4c64eb14.png"
+  },
+  {
+    "revision": "5551d8999184ae33d0010da44bf17036",
+    "url": "/Portfolio/static/media/Portfolio.5551d899.png"
+  },
+  {
+    "revision": "e197b0c98263d8a8bc35391e135edf51",
+    "url": "/Portfolio/static/media/Shkelqim.e197b0c9.jpg"
+  }
+]);
